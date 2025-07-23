0fa12656f0896365cf870282884dad788e66915e
Gustav Hultgren <gustav.hultgren@resurs.se>
2025-07-23 14:22:12 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        sand: '#f1ebe6',
+        textColor: '#1f221d'
+      }
+    },
+  },
+  plugins: [],
+}
